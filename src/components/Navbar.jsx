f14361c1af5ae138aec2795306e6f3064dcbc91b
Steven Matthew Revillas <stevenmatthewr@gmail.com>
2023-05-26 01:20:02 +0800

    adjustment to navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import Brand from '../assets/images/brand.jpg';
 
+const HOME = window.location.href;
+
 export default function Navbar () {
     return (
         <>
             <nav class="navbar navbar-expand-lg" style={{ backgroundColor: "rgba(94, 203, 255, 1)" }}>
                 <div class="container-fluid">
-                    <a class="navbar-brand text-light" href="http://localhost:3000">
+                    <a class="navbar-brand text-light" href={HOME}>
                         <img src={Brand} alt="Steven" width={40} height={40}></img>
                     </a>
                     <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,19 +16,19 @@ export default function Navbar () {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
                             <li class="nav-item">
-                                <a class="nav-link text-white active" aria-current="page" href="http://localhost:3000">Projects</a>
+                                <a class="nav-link text-white active" aria-current="page" href={HOME}>Projects</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="http://localhost:3000">Contact Me!</a>
+                                <a class="nav-link text-white" href={HOME}>Contact Me!</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link text-white dropdown-toggle" href="http://localhost:3000" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link text-white dropdown-toggle" href={HOME} role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Documents
                                 </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="http://localhost:3000">Action</a></li>
-                                <li><a class="dropdown-item" href="http://localhost:3000">Another action</a></li>
-                                <li><a class="dropdown-item" href="http://localhost:3000">Something else here</a></li>
+                                <li><a class="dropdown-item" href={HOME}>Action</a></li>
+                                <li><a class="dropdown-item" href={HOME}>Another action</a></li>
+                                <li><a class="dropdown-item" href={HOME}>Something else here</a></li>
                             </ul>
                             </li>
                         </ul>
