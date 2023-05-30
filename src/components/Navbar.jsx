7b88d40452c60041a8da2d6fb34953f4c9d576c8
Steven Matthew Revillas <stevenmatthewr@gmail.com>
2023-05-30 11:13:38 +0800

    applied props to changeable data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,46 @@
 import Brand from '../assets/images/brand.jpg';
+import { STEVEN } from '../data';
 
 const HOME = window.location.href;
+
+let steven = STEVEN;
 
 export default function Navbar () {
     return (
         <>
-            <nav class="navbar navbar-expand-lg" style={{ backgroundColor: "rgba(94, 203, 255, 1)" }}>
-                <div class="container-fluid">
-                    <a class="navbar-brand text-light" href={HOME}>
-                        <img src={Brand} alt="Steven" width={40} height={40}></img>
+            <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "rgba(94, 203, 255, 1)" }}>
+                <div className="container-fluid">
+                    <a className="navbar-brand text-light" href={HOME}>
+                        <h6> <img src={Brand} alt="Steven" width={40} height={40}></img>  {steven.firstName}</h6>
                     </a>
-                    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link text-white active" aria-current="page" href={HOME}>Projects</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div className="col">
+
+                        </div>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
+                            <li className="nav-item">
+                                <a className="nav-link text-white active" aria-current="page" href={HOME}>Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href={HOME}>Contact Me!</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-white active" aria-current="page" href={HOME}>About me</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link text-white dropdown-toggle" href={HOME} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item">
+                                <a className="nav-link text-white active" aria-current="page" href={HOME}>Projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-white" href={HOME}>Contact Me!</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link text-white dropdown-toggle" href={HOME} role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Documents
                                 </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href={HOME}>Resume</a></li>
-                                <li><a class="dropdown-item" href={HOME}>Reccomendation Letter</a></li>
-                                <li><a class="dropdown-item" href={HOME}>MOA</a></li>
+                            <ul className="dropdown-menu dropdown-menu-end">
+                                <li><a className="dropdown-item" href={HOME}>Resume</a></li>
+                                <li><a className="dropdown-item" href={HOME}>Reccomendation Letter</a></li>
+                                <li><a className="dropdown-item" href={HOME}>MOA</a></li>
                             </ul>
                             </li>
                         </ul>
---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const STEVEN = {
+    firstName: 'Steven Matthew',
+    middleName: 'Solis',
+    lastName: 'Revillas',
+    age: '22',
+    bDay: '12/13/2000',
+    address: 'BLK 147 LOT 16, Mangondato St., Central Bicutan Taguig City, Philippines 1633',
+    profession: 'Web Developer',
+    school: 'Polytechnic University of the Philippines Taguig',
+    github: 'https://github.com/7Matthew',
+    linkedin: 'https://www.linkedin.com/in/steven-matthew-revillas-6a3467236/',
+    facebook:'https://www.facebook.com/itsmeSMR'
+};
