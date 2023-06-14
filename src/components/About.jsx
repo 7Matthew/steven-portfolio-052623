51964d4414f37831fbe95404063e6cce82bba187
Steven Matthew Revillas <stevenmatthewr@gmail.com>
2023-06-15 02:19:27 +0800

    Added technologies subsection

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
 import { STEVEN } from "../data"; 
 import Divider from "./Divider";
+import {HtmlSvg, CssSvg, JavascriptSvg, BootstrapSvg, FontawesomeSvg, JquerySvg, JavaSvg, LaravelSvg, ReactSvg, GithubSvg, MySqlSvg, XamppSvg} from "./Svgs"
 import { isSmall, isLarge, isMedium, isXSmall } from "./Projects";
 import profilePic from "../assets/images/profilePic.png"
 import "../assets/css/about.css";
+import React from "react";
 
 /** 
  * TODO: Introduce yourself. 
@@ -21,71 +23,156 @@ export default function About(){
         <>  
             <Divider title ="About me"  id="about-me" />
             <div className="container my-5 rounded">
-                <div className="row">
-                    <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 p-2">
-                        <div className="container text-center bg-white" data-aos="fade-down">
-                            <img src={profilePic} alt="" width={"80%"} className="profile-pic rounded"/>
-                        </div>  
-                    </div>
-                    <div className={
-                        isLarge ? "col-lg-6 col-md-12 col-sm-12 col-xs-12" : 
-                        isMedium ? "col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center" : 
-                        isSmall ? "col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center" : 
-                        isXSmall ? "col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center" :
-                        "col-lg-6 col-md-12 col-sm-12 col-xs-12" 
-                    }> 
-                        <div className="container-fluid bg-white py-5" data-aos="fade-left">
-                            <div className="row">
-                                <h3>
-                                    <p className="text-dark" style={{fontWeight:"bolder", lineHeight:"2.5rem"}}>
-                                        <b className="text-info">{steven.firstName} </b>is a Web Developer who has a comprehensive experience in Front-End and Back-End Development.
-                                    </p>
-                                </h3>
-                            </div>
-                            <div className="row">
-                                <h6>
-                                    <p className="text-secondary" style={{fontWeight:"bolder", lineHeight:"2rem"}}>
-                                        He considers himself as a builder and a logician. He is fascinated by the way his works come into view, and how it provides solutions. He has an academic background in Information Technology that helped him to acquire skills such as  <u>Project</u> <u>Management</u>, <u>Back-End</u>, and <u>Front-End Development</u>.
-                                    </p>
-                                </h6>
-                            </div>
-                            <div className="row">
-                                <h6>
-                                    <p className="text-secondary" style={{fontWeight:"bolder", lineHeight:"2rem"}}>
-                                        He recently completed projects that allowed him to be trained in Full Stack Development. He specializes in Laravel framework for Full Stack Web Development with the use of MySql database.
-                                    </p>
-                                </h6>
-                            </div>
+                <Introduction />
+                <Experiences />
+                <Technologies />
+            </div>
+        </>
+    );
+}
+
+function Introduction (){
+    return(
+        <>
+            <div className="row">
+                <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 p-2">
+                    <div className="container text-center bg-white" data-aos="fade-down">
+                        <img src={profilePic} alt="" width={"80%"} className="profile-pic rounded"/>
+                    </div>  
+                </div>
+                <div className={
+                    isLarge ? "col-lg-6 col-md-12 col-sm-12 col-xs-12" : 
+                    isMedium ? "col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center" : 
+                    isSmall ? "col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center" : 
+                    isXSmall ? "col-lg-6 col-md-12 col-sm-12 col-xs-12 text-center" :
+                    "col-lg-6 col-md-12 col-sm-12 col-xs-12" 
+                }> 
+                    <div className="container-fluid bg-white py-5" data-aos="fade-left">
+                        <div className="row">
+                            <h3>
+                                <p className="text-dark" style={{fontWeight:"bolder", lineHeight:"2.5rem"}}>
+                                    <b className="text-info">{steven.firstName} </b>is a Web Developer who has a comprehensive experience in Front-End and Back-End Development.
+                                </p>
+                            </h3>
+                        </div>
+                        <div className="row">
+                            <h6>
+                                <p className="text-secondary" style={{fontWeight:"bolder", lineHeight:"2rem"}}>
+                                    He considers himself as a builder and a logician. He is fascinated by the way his works come into view, and how it provides solutions. He has an academic background in Information Technology that helped him to acquire skills such as  <u>Project</u> <u>Management</u>, <u>Back-End</u>, and <u>Front-End Development</u>.
+                                </p>
+                            </h6>
+                        </div>
+                        <div className="row">
+                            <h6>
+                                <p className="text-secondary" style={{fontWeight:"bolder", lineHeight:"2rem"}}>
+                                    He recently completed projects that allowed him to be trained in Full Stack Development. He specializes in Laravel framework for Full Stack Web Development with the use of MySql database.
+                                </p>
+                            </h6>
                         </div>
                     </div>
                 </div>
-                <div className="row mt-5">
-                    <div className="col-lg-12 text-center mt-5">
-                        <h1><b>Experiences</b></h1>
+            </div>
+        </>
+    );
+}
+
+function Experiences (){
+    return(
+        <>
+            <div className="row mt-5">
+                <div className="col-lg-12 text-center mt-5">
+                    <h1><b>Experiences</b></h1>
+                </div>
+            </div>
+            <div className="row text-center p-5 mb-5">
+                <div className="col-lg-4 col-md-12 col-sm-12 my-4">
+                    <div className="card border-1  p-5 rounded" data-aos="fade-up" >
+                        <h1><i className="fa-solid fa-list-check text-info fa-xl mb-5"></i></h1>
+                        <h3><b>Project Management</b></h3>
+                        <p> Planned, executed, and oversaw projects. Ensured timely completion of deliverables, within budget, and meet quality standards.  </p>
                     </div>
                 </div>
-                <div className="row text-center p-5 mb-5">
-                    <div className="col-lg-4 col-md-12 col-sm-12 my-4">
-                        <div className="card border-1  p-5 rounded" data-aos="fade-up" >
-                            <h1><i className="fa-solid fa-list-check text-info fa-xl mb-5"></i></h1>
-                            <h3><b>Project Management</b></h3>
-                            <p> Planned, executed, and oversaw projects. Ensured timely completion of deliverables, within budget, and meet quality standards.  </p>
-                        </div>
+                <div className="col-lg-4 col-md-12 col-sm-12 my-4">
+                    <div className="card border-1  p-5 rounded" data-aos="fade-up" data-aos-delay="200">
+                        <h1><i className="fa-solid fa-display text-info fa-xl mb-5"></i></h1>
+                        <h3><b>Front-End Developer</b></h3>
+                        <p> Planned and executed an engaging user experience by implementing responsive layouts, smooth animations, and interactive features.</p>
                     </div>
-                    <div className="col-lg-4 col-md-12 col-sm-12 my-4">
-                        <div className="card border-1  p-5 rounded" data-aos="fade-up" data-aos-delay="200">
-                            <h1><i className="fa-solid fa-display text-info fa-xl mb-5"></i></h1>
-                            <h3><b>Front-End Developer</b></h3>
-                            <p> Planned and executed an engaging user experience by implementing responsive layouts, smooth animations, and interactive features.</p>
-                        </div>
+                </div>
+                <div className="col-lg-4 col-md-12 col-sm-12 my-4">
+                    <div className="card border-1  p-5 rounded" data-aos="fade-up" data-aos-delay="500">
+                        <h1><i className="fa-solid fa-gears text-info fa-xl mb-5"></i></h1>
+                        <h3><b>Back-End Developer</b></h3>
+                        <p> Planned the designs and implements the logic and functionality that powers the website behind the scenes. Focused on the server-side functionality.</p>
                     </div>
-                    <div className="col-lg-4 col-md-12 col-sm-12 my-4">
-                        <div className="card border-1  p-5 rounded" data-aos="fade-up" data-aos-delay="500">
-                            <h1><i className="fa-solid fa-gears text-info fa-xl mb-5"></i></h1>
-                            <h3><b>Back-End Developer</b></h3>
-                            <p> Planned the designs and implements the logic and functionality that powers the website behind the scenes. Focused on the server-side functionality.</p>
-                        </div>
-                    </div>
+                </div>
+            </div>     
+        </>
+    );
+}
+
+function Technologies (){
+    return (
+        <>
+            <div className="row mt-3">
+                <div className="col-lg-12 text-center mt-5">
+                    <h1><b>Technologies</b></h1>
+                </div>
+            </div>
+            <div className="row text-center mt-5">
+                <div className="col">
+                    <HtmlSvg />
+                    <h3><b>HTML</b></h3>
+                </div>
+                <div className="col">
+                    <CssSvg />
+                    <h3><b>CSS</b></h3>
+                </div>
+                <div className="col">
+                    <JavascriptSvg />
+                    <h3><b>JavaScript</b></h3>
+                </div>
+            </div>
+            <div className="row text-center mt-5">
+                <div className="col">
+                    <BootstrapSvg size="70px"/>
+                    <h3><b>Bootstrap</b></h3>
+                </div>
+                <div className="col">
+                    <FontawesomeSvg size="70px"/>
+                    <h3><b>Fontawesome</b></h3>
+                </div>
+                <div className="col">
+                    <JquerySvg size="70px"/>
+                    <h3><b>JQuery</b></h3>
+                </div>
+            </div>
+            <div className="row text-center mt-5">
+                <div className="col">
+                    <JavaSvg size="70px"/>
+                    <h3><b>Java</b></h3>
+                </div>
+                <div className="col">
+                    <LaravelSvg size="70px"/>
+                    <h3><b>Laravel</b></h3>
+                </div>
+                <div className="col">
+                    <ReactSvg size="70px"/>
+                    <h3><b>React</b></h3>
+                </div>
+            </div>
+            <div className="row text-center mt-5">
+                <div className="col">
+                    <GithubSvg size="70px"/>
+                    <h3><b>Github</b></h3>
+                </div>
+                <div className="col">
+                    <MySqlSvg size="70px"/>
+                    <h3><b>MySQL</b></h3>
+                </div>
+                <div className="col">
+                    <XamppSvg size="70px"/>
+                    <h3><b>Xampp</b></h3>
                 </div>
             </div>
         </>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 
 import Divider from "./Divider";
+import nonongs from "../assets/images/nonongs.jpg"
 
 export const isXSmall = (window.innerWidth < 576);
 export const isSmall = (window.innerWidth >= 576) && (window.innerWidth <= 767);
@@ -61,7 +62,7 @@ function Nonongs(){
                     <div className="col-lg-6 mt-3 mb-3" >
                         <a href="https://nonongs.puptcapstone.net" target='__blank' title='Visit the site!'>
                             <img 
-                                src={'https://scontent.fmnl17-2.fna.fbcdn.net/v/t39.30808-6/301791319_554823959778327_3904387206828962428_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH00orolgEOjio_YRCGzYauKaNUJa5qOcYpo1Qlrmo5xsgXMtxaNzKZ307463UPsMfoU-WL2PZnej8t4khgONr8&_nc_ohc=jfy4r1q9rRwAX9RvlfI&_nc_ht=scontent.fmnl17-2.fna&oh=00_AfAPsYr9ND1Fqh4emsomGrQ-aA1513HcX69sBWrLEtYgYA&oe=6486768F'} 
+                                src={nonongs} 
                                 alt="Pos System" 
                                 className='rounded-circle'
                                 style={{width:"60%", height:"100%", boxShadow: "5px 5px rgba(0,0,0, .1)"}}
